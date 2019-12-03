a2eccb89c21e6e42b5d3542bf71db2ae66cfe951
Zachariah Brown <zbrown@hubspot.com>
2019-12-02 23:29:22 -0500

    Restructure for years.

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var fr = require('./fileReader.js');
+
+// Set the day number to the day of the month for the associated problem
+var dayNumber = 1;
+var year = 2018;
+
 function runForDay(day) {
-    var input = fr.readLines(`./inputs/${dayNumber}.txt`);
-    var day = require(`./days/${dayNumber}.js`);
+    var input = fr.readLines(`./years/${year}/inputs/${dayNumber}.txt`);
+    var day = require(`./years/${year}/days/${dayNumber}.js`);
 
     var output1 = day.partOne(input);
     var output2 = day.partTwo(input);
 
-    console.log(`---- Day ${dayNumber} ----`);
+    console.log(`---- Day ${dayNumber} ${year} ----`);
     console.log(`Part 1: ${output1}`);
     console.log(`Part 2: ${output2}`);
 }
 
-// Set the day number to the day of the month for the associated problem
-var dayNumber = 1;
 
 if (dayNumber === undefined) {
     console.warn('Must set problem day number.');
