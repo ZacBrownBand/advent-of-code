0be0a81066cc07e4d14f9f654f77585cea39c66c
Zachariah Brown <zbrown@hubspot.com>
2019-12-04 10:49:59 -0500

    Day 04

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var fr = require('./fileReader.js');
 
 // Set the day number to the day of the month for the associated problem
-var dayNumber = 3;
+var dayNumber = 4;
 var year = 2019;
 
 function runForDay(day) {
