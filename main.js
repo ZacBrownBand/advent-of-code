9c134708b101728d14cb63fa63ba93957c71da34
Zachariah Brown <zbrown@hubspot.com>
2019-12-06 01:27:50 -0500

    Udpates main.js

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-var fr = require('./fileReader.js');
+const fr = require('./fileReader.js');
 
 // Set the day number to the day of the month for the associated problem
-var dayNumber = 6;
-var year = 2019;
+const dayNumber = 6;
+const year = 2019;
 
-function runForDay(day) {
-    var input = fr.readLines(`./years/${year}/inputs/${dayNumber}.txt`);
-    var day = require(`./years/${year}/days/${dayNumber}.js`);
+const runForDay = day => {
+  const input = fr.readLines(`./years/${year}/inputs/${day}.txt`);
+  const { partOne, partTwo } = require(`./years/${year}/days/${day}.js`);
 
-    var output1 = day.partOne(input);
-    var output2 = day.partTwo(input);
+  const partOneOutput = partOne(input);
+  const partTwoOutput = partTwo(input);
 
-    console.log(`---- Day ${dayNumber} ${year} ----`);
-    console.log(`Part 1: ${output1}`);
-    console.log(`Part 2: ${output2}`);
+  console.log(`---- Day ${dayNumber} ${year} ----`);
+  console.log(`Part 1: ${partOneOutput}`);
+  console.log(`Part 2: ${partTwoOutput}`);
 }
 
+const dayNumberAdjusted = dayNumber < 10 ? '0' + dayNumber : dayNumber;
 
-if (dayNumber === undefined) {
-    console.warn('Must set problem day number.');
-    return;
-}
-
-if (dayNumber < 10) {
-	dayNumber = '0' + dayNumber;
-}
-
-runForDay(dayNumber);
+runForDay(dayNumberAdjusted);
