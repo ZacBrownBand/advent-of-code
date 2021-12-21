464c0e1f3c15581541e63bdcf23ab1691230a915
Zachariah Brown <zachariahwbrown1@gmail.com>
2021-12-21 01:03:18 -0500

    added template for each day

---
+++ years/2021/days/template.js
@@ -0,0 +1,17 @@
+const { linesFromRead } = require("../../../fileReader.js");
+
+const parseInput = (input) => linesFromRead(input);
+
+const partOne = (lines) => {};
+const partTwo = (lines) => {};
+
+module.exports = {
+  beforeAll: Function.prototype,
+  beforeEach: Function.prototype,
+  parseInput,
+  partOne,
+  partTwo,
+  testing: true,
+  runPartOne: true,
+  runPartTwo: true,
+};
