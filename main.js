76e748b401feda2f47001e8cfe9f2e423226e9a0
Zachariah Brown <ZacBrownBand@users.noreply.github.com>
2022-12-03 02:04:21 -0500

    Zb/day3 (#5)
    * day 2
    
    * Prepare for day 3
    
    * Day 3 input
    
    * Prototype functions for sets
    
    * Day 3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const prototypeFunctions = require("./prototypeFunctions.js");
 
 // Set the day number to the day of the month for the associated problem
 
-const dayNumber = 02;
+const dayNumber = 03;
 const year = 2022;
 
 const runForDay = (day) => {
