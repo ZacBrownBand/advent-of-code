afad5e2d102af7744bfcdc6cde4cf5da36778986
Zachariah Brown <zachariahwbrown1@gmail.com>
2022-12-04 00:45:45 -0500

    Prep for day 5

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const prototypeFunctions = require("./prototypeFunctions.js");
 
 // Set the day number to the day of the month for the associated problem
 
-const dayNumber = 04;
+const dayNumber = 05;
 const year = 2022;
 
 const runForDay = (day) => {
