e0e80576d234b8571af99e0ec03d3efc63bf9ed4
Zachariah Brown <zachariahwbrown1@gmail.com>
2022-12-05 00:55:04 -0500

    Day 05

--- years/2022/days/05.js
+++ years/2022/days/05.js
@@ -1,22 +1,66 @@
 const parseInput = (input) => {
-  return input.split('\n');
+  const lines = input.split('\n');
+
+  const moves = lines.filter(line => line[0] === 'm').map(move => {
+    const nums = move.replace('move ', '').replace(' from ', ',').replace(' to ', ',').split(',').map(Number);
+    return {
+      count: nums[0],
+      from:  nums[1] - 1,
+      to:  nums[2] - 1,
+    };
+  })
+  
+  const rowsNumbers = lines.filter(line => line[1] === '1')[0].split(' ').filter(a => a !== '');
+  const numRows = parseInt(rowsNumbers[rowsNumbers.length - 1]);
+
+  const state = Array(numRows).fill([]);
+  const containers = lines.filter(line => line[0] === '[' || (line[0] === ' ' && line[1] !== '1'  && line.length));
+   
+  for (let i = containers.length - 1; i >=0; i--) {
+    for (let col = 1; col < numRows * 4; col+=4) {
+      const box = containers[i][col];
+      if (box !== ' ') {
+        state[(col - 1) / 4].push(box)
+      }
+    }
+  }
+
+  return { 
+    moves,
+    state
+  }
+
 };
 
-const partOne = (lines) => {
-  return 0;
+const partOne = ({moves, state}) => {
+  const containers = JSON.parse(JSON.stringify(state));
+  moves.forEach((move) => {
+      for (let i = 0; i < move.count; i++) {
+        containers[move.to].push(containers[move.from].pop())
+      }
+  });
+  return containers.map(stack => stack.pop()).join('');
 };
 
-const partTwo = (lines) => {
-  return 0;
+const partTwo = ({moves, state}) => {
+  const containers = JSON.parse(JSON.stringify(state));
+  moves.forEach((move) => {
+    const pivot = [];
+    for (let i = 0; i < move.count; i++) {
+      pivot.push(containers[move.from].pop())
+    }
+    for (let i = 0; i < move.count; i++) {
+      containers[move.to].push(pivot.pop())
+    }
+  });
+  return containers.map(stack => stack.pop()).join('');
 };
 
 module.exports = {
-  // beforeAll, // optional - function to run before the solutions are run
-  // beforeEach, // optional - function to run before each solution
   parseInput,
   partOne,
   partTwo,
-  testing: true,
+  testing: false,
   runPartOne: true,
   runPartTwo: true,
 };
