fb2510f583ada2c82eb39a7d9902e389c12f191c
Zachariah Brown <ZacBrownBand@users.noreply.github.com>
2022-12-09 02:16:55 -0500

    Day 9 (#11)
    * Day 9 prep
    
    * Day 9

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const prototypeFunctions = require("./prototypeFunctions.js");
 
 // Set the day number to the day of the month for the associated problem
 
-const dayNumber = 08;
+const dayNumber = 09;
 const year = 2022;
 
 const runForDay = (day) => {
