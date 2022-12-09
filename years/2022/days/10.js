581147ba306abf5a39e5f0dc6c7db1d026d32e0b
Zachariah Brown <zachariahwbrown1@gmail.com>
2022-12-09 02:18:36 -0500

    Day 10 prep

---
+++ years/2022/days/10.js
@@ -0,0 +1,22 @@
+const parseInput = (input) => {
+  return input.split('\n');
+};
+
+const partOne = (lines) => {
+  return 0;
+};
+
+const partTwo = (lines) => {
+  return 0;
+};
+
+module.exports = {
+  // beforeAll, // optional - function to run before the solutions are run
+  // beforeEach, // optional - function to run before each solution
+  parseInput,
+  partOne,
+  partTwo,
+  testing: true,
+  runPartOne: true,
+  runPartTwo: true,
+};
